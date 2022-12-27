86377ae4a68f44b13eaf7f2e5b9c0960b6bd6e7d
Moodybleu <meganbenn27@icloud.com>
2022-12-27 14:53:10 -0800

    finished project - vowel counter

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ btn.addEventListener("click", countVowel);
 
 function countVowel() {
     let vowelCount = 0;
-    let wordVal = word.ariaValueMax.toLowerCase();
-    
-    for ( let i = 0; i < wordVal. length; i++) {
-        let letter = wordVal[i];
-        if (letter.match(/([a,e,i,o,u])/)) {
-            vowelCount++;
-        }
+    let wordVal = word.value.toLowerCase();
+  
+    for (let i = 0; i < wordVal.length; i++) {
+      let letter = wordVal[i];
+      if (letter.match(/([a,e,i,o,u])/)) {
+        vowelCount++;
+      }
     }
-}
-
+    result.innerHTML = `${word.value.toUpperCase()} has ${vowelCount} vowels`;
+  }
